ed3e2880af4b04decdd0410faf6b8fbd6d225dde
Lucie <lucka2121@gmail.com>
2018-09-20 12:01:05 +0200

    upgrate MainActivity.class

--- app/src/main/java/com/example/lucie/mygame1/MainActivity.java
+++ app/src/main/java/com/example/lucie/mygame1/MainActivity.java
@@ -1,15 +1,37 @@
 package com.example.lucie.mygame1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button hrat;
+    private Button konec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("test");
+
+        hrat = findViewById(R.id.buttonHrat);
+        hrat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), HraActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+        konec = findViewById(R.id.buttonKonec);
+        konec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(1);
+            }
+        });
     }
 }
