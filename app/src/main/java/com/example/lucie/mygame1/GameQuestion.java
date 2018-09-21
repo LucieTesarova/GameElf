079824695cb870f9bed29e828c07ff13a60f5bd1
Lucie <lucka2121@gmail.com>
2018-09-21 12:17:51 +0200

    create class GameQuestion

---
+++ app/src/main/java/com/example/lucie/mygame1/GameQuestion.java
@@ -0,0 +1,14 @@
+package com.example.lucie.mygame1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameQuestion extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.gameQuestion);
+    }
+}
