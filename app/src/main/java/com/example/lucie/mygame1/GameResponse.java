fdd9f5793c457afd172647b1ddf92e255f333c92
Lucie <lucka2121@gmail.com>
2018-09-22 15:18:44 +0200

    create method onRadioButtonClicked in GameResponse

--- app/src/main/java/com/example/lucie/mygame1/GameResponse.java
+++ app/src/main/java/com/example/lucie/mygame1/GameResponse.java
@@ -3,7 +3,9 @@ package com.example.lucie.mygame1;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class GameResponse extends Activity {
 
@@ -18,6 +20,22 @@ public class GameResponse extends Activity {
         ano = findViewById(R.id.radiobuttonano);
         ne = findViewById(R.id.radiobuttonne);
 
+    }
 
+    public void onRadioButtonClicked(View v) {
+        boolean checked = ((RadioButton) v).isChecked();
+
+        switch (v.getId()) {
+            case R.id.radiobuttonano:
+                if (checked) {
+                    Toast.makeText(this, "ano", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.radiobuttonne:
+                if (checked) {
+                    Toast.makeText(this, "ne", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 }
