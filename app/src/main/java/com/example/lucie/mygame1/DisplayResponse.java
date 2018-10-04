50f117aeeb93903da26618002a185a8de0282a25
John Doe <lucka2121@gmail.com>
2018-10-04 19:00:00 +0200

    update layout

--- app/src/main/java/com/example/lucie/mygame1/DisplayResponse.java
+++ app/src/main/java/com/example/lucie/mygame1/DisplayResponse.java
@@ -3,17 +3,28 @@ package com.example.lucie.mygame1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DisplayResponse extends Activity {
 
     private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_response);
         textView = findViewById(R.id.response);
+        button = findViewById(R.id.buttonPotvrdit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GameQuestion.class);
+                startActivity(intent);
+            }
+        });
 
         Intent intent = getIntent();
         String response = intent.getStringExtra("odkaz");
