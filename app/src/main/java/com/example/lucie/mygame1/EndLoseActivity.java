da400f9e46d2b869baa86b599ce68345a3159e35
John Doe <lucka2121@gmail.com>
2018-11-29 20:29:52 +0100

    create animation

--- app/src/main/java/com/example/lucie/mygame1/EndLoseActivity.java
+++ app/src/main/java/com/example/lucie/mygame1/EndLoseActivity.java
@@ -3,15 +3,50 @@ package com.example.lucie.mygame1;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnticipateInterpolator;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class EndLoseActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_lose);
+        imageView = findViewById(R.id.image_prohraljsi);
         overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+
         Toast.makeText(getApplicationContext(), "Otevrena endLoseActivity", Toast.LENGTH_LONG).show();
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            rotace.run();
+        }
+    }
+
+    Runnable rotace = new Runnable() {
+        @Override
+        public void run() {
+            imageView.animate().setDuration(1500)
+                    .setInterpolator(new AccelerateInterpolator())
+                    .rotationBy(1080)
+                    .withEndAction(zvetseni);
+        }
+    };
+
+    Runnable zvetseni = new Runnable() {
+        @Override
+        public void run() {
+            imageView.animate().setDuration(1000)
+                    .setInterpolator(new AnticipateInterpolator())
+                    .scaleXBy(1.0f)
+                    .scaleYBy(1.0f);
+        }
+    };
 }
--- app/src/main/java/com/example/lucie/mygame1/EndWinActivity.java
+++ app/src/main/java/com/example/lucie/mygame1/EndWinActivity.java
@@ -2,15 +2,99 @@ package com.example.lucie.mygame1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.OvershootInterpolator;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class EndWinActivity extends AppCompatActivity {
+
+    private ImageView imageVyhra;
+    private ImageView hvezda1;
+    private ImageView hvezda2;
+    private ImageView hvezda3;
+    private ImageView hvezda4;
+    private ImageView hvezda5;
+    private ImageView hvezda6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_win);
         overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+
+        imageVyhra = findViewById(R.id.image_vyhraljsi);
+        hvezda1 = findViewById(R.id.hvezda1);
+        hvezda2 = findViewById(R.id.hvezda2);
+        hvezda3 = findViewById(R.id.hvezda3);
+        hvezda4 = findViewById(R.id.hvezda4);
+        hvezda5 = findViewById(R.id.hvezda5);
+        hvezda6 = findViewById(R.id.hvezda6);
+
+
         Toast.makeText(getApplicationContext(), "Otevrena endWinActivity", Toast.LENGTH_LONG).show();
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            pruhlednost.run();
+        }
+    }
+
+    Runnable pruhlednost = new Runnable() {
+        @Override
+        public void run() {
+            hvezda1.animate().setDuration(1500)
+                    .setInterpolator(new LinearInterpolator()).alpha(1.0f);
+            hvezda2.animate().setDuration(2300)
+                    .setInterpolator(new LinearInterpolator()).alpha(1.0f);
+            hvezda3.animate().setDuration(2000)
+                    .setInterpolator(new LinearInterpolator()).alpha(1.0f);
+            hvezda4.animate().setDuration(2400)
+                    .setInterpolator(new LinearInterpolator()).alpha(1.0f);
+            hvezda5.animate().setDuration(2300)
+                    .setInterpolator(new LinearInterpolator()).alpha(1.0f);
+            hvezda6.animate().setDuration(1500)
+                    .setInterpolator(new LinearInterpolator()).alpha(1.0f);
+            imageVyhra.animate().setDuration(2900)
+                    .setInterpolator(new LinearInterpolator()).alpha(1.0f)
+                    .withEndAction(rotace);
+        }
+    };
+
+    Runnable rotace = new Runnable() {
+        @Override
+        public void run() {
+            hvezda1.animate().setDuration(500)
+                    .setInterpolator(new AccelerateInterpolator()).rotationBy(360.0f);
+            hvezda2.animate().setDuration(500)
+                    .setInterpolator(new AccelerateInterpolator()).rotationBy(360.0f);
+            hvezda3.animate().setDuration(500)
+                    .setInterpolator(new AccelerateInterpolator()).rotationBy(360.0f);
+            hvezda4.animate().setDuration(500)
+                    .setInterpolator(new AccelerateInterpolator()).rotationBy(360.0f);
+            hvezda5.animate().setDuration(500)
+                    .setInterpolator(new AccelerateInterpolator()).rotationBy(360.0f);
+            hvezda6.animate().setDuration(500)
+                    .setInterpolator(new AccelerateInterpolator()).rotationBy(360.0f);
+            imageVyhra.animate().setDuration(1000)
+                    .setInterpolator(new AccelerateInterpolator())
+                    .rotationBy(1080.0f)
+                    .withEndAction(zvetseni);
+        }
+    };
+
+    Runnable zvetseni = new Runnable() {
+        @Override
+        public void run() {
+            imageVyhra.animate().setDuration(1000)
+                    .setInterpolator(new AnticipateInterpolator())
+                    .scaleXBy(1.0f)
+                    .scaleYBy(1.0f);
+        }
+    };
 }
